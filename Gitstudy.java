4194deae794c4b60de264bf449d7055396bf1411
dhpahk <dhpahk@gmail.com>
2017-03-22 03:59:30 +0900

    @ 4rd commit

--- Gitstudy.java
+++ Gitstudy.java
@@ -2,7 +2,6 @@ class Gitstudy
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Git! Study!");
-		System.out.println("This is second commit!");
+		System.out.println("This is 4rd commit!");
 	}
 }
